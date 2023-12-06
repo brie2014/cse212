1d140be24bf9ffb5102d1b87433cc0143ba559f4
Brienna Sambrano <brie2014@byui.edu>
2023-12-05 23:50:43 -0700

    week 06 binary trees prove

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,28 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        // If the value already exists, don't add it
+        if (value == Data) return;
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -24,13 +31,41 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
+
         // TODO Start Problem 2
-        return false;
+        if (value == Data) return true;
+        else if (value < Data)
+        {
+            // Search to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            // Search to the right
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
-    public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+    public int GetHeight()
+    {
+        if (Left == null && Right == null) return 1;
+        else if (Left == null && Right != null)
+        {
+            return 1 + Right.GetHeight();
+        }
+        else if (Right == null && Left != null)
+        {
+            return 1 + Left.GetHeight();
+        }
+        else
+            return 1 + Math.Max(Right.GetHeight(), Left.GetHeight());
     }
 }
